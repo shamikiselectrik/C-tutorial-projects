bf932abb55b0f63f71f446c98831184b91deed4d
Shamik Mahapatra <221411562+shamikiselectrik@users.noreply.github.com>
2026-05-25 12:15:04 +0530

    Add files via upload

---
+++ BitOperations.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (){
+    unsigned int a = 60;
+    unsigned int b = 13;
+    int result = 0;
+
+    result = a | b;
+
+    printf("result is %d", result);
+
+    return 0;
+}
